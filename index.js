6157e45184eb3d853c0e49a2de028f4b68339b63
Alex Milanov <alex@i4web.biz>
2021-04-27 22:01:01 +0300

    show data for last 7 days

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const pug = require('pug');
 const request = require('superagent');
+
 const url = 'https://data.egov.bg/api/getResourceData';
 const resource_uri = 'cb5d7df0-3066-4d7a-b4a1-ac26525e0f0c';
+// show data for {days} amount of days before current
+const days = 7;
+const region = 'SOF_ACT';
 
 const getData = () => request
   .post(url)
@@ -9,12 +13,12 @@ const getData = () => request
   .set('Accept', 'application/json')
   .then(res => res.body.data)
   .then(data => ({
-    index: data[0].findIndex(r => r === 'SOF_ACT'),
+    index: data[0].findIndex(r => r === region), // SOF_ACT
     data
   }))
   .then(({index, data}) => ({
     current: data.slice(-1).pop()[index],
-    data: data.slice(-4, -1).map(r => [r[0], r[index]])
+    data: data.slice(-(days + 1), -1).map(r => [r[0], r[index]])
   }));
 
 module.exports = () => getData().then(data => pug.renderFile('index.pug', data));
